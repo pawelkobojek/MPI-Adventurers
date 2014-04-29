20e9fb9055dad577a7e5d60ce8abce4138dbbef8
Pawel Kobojek <kobojekp@omega.mini.pw.edu.pl>
2014-04-29 21:18:27 +0200

    Init

---
+++ Makefile
@@ -0,0 +1,25 @@
+CC=mpicc
+C_FLAGS=-Wall  -g
+L_FLAGS=-lm -lrt
+TARGET=prlab3
+FILES=main.o
+SOURCE=main.c
+HOSTFILE=hosts
+N=6
+K=3
+MAP_FILE=./mapa
+
+${TARGET}: ${FILES}
+	${CC} -o ${TARGET} ${FILES} ${L_FLAGS}
+
+${FILES}: ${SOURCE}
+	${CC} -o ${FILES} -c ${SOURCE} ${C_FLAGS}
+
+.PHONY: clean run
+
+run:
+	mpirun --hostfile ${HOSTFILE} ./${TARGET} ${N} ${K} ${MAP_FILE}
+	
+clean:
+	-rm -f ${FILES} ${TARGET}
+ 
---
+++ main.c
@@ -0,0 +1,116 @@
+#define _GNU_SOURCE 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "mpi.h"
+
+#define ERR(source) (fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
+                     perror(source),kill(0,SIGKILL),\
+                         exit(EXIT_FAILURE))
+
+#define MAX_SLEEP 2
+#define MIN_SLEEP 1
+
+#define BUFF_SIZE 32
+
+#define COORDS_SEPARATOR ','
+
+typedef struct {
+	int x;
+	int y;
+} location;
+
+int main(int argc, char* argv[]) {
+	int rank, size, n=4, k=3, num=0, i, blockcounts[1], total_wrecks, found_wrecks=0;
+	location* locs;
+	location* rec;
+	MPI_Datatype locationtype, oldtypes[1];
+	MPI_Aint offsets[1];
+	
+	/* Initialize the environment */
+	MPI_Init( &argc, &argv );
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+	n=size;
+	offsets[1] = 0;
+	blockcounts[0] = 2;
+	oldtypes[0] = MPI_INT;
+	MPI_Type_struct(1, blockcounts, offsets, oldtypes, &locationtype);
+	MPI_Type_commit(&locationtype);
+	if( (rec = (location*) calloc(k, sizeof(location))) == NULL) {
+		ERR("calloc");
+	}
+
+	if( (locs = (location*) calloc(n * k, sizeof(location))) == NULL) {
+		ERR("calloc");
+	}
+	srand(getpid());
+	if(rank == 0) {
+		FILE* file;
+//		if( (fd = TEMP_FAILURE_RETRY(open(argv[3], O_RDONLY))) == -1) {
+//			ERR("open");
+//		}
+
+//		if(TEMP_FAILURE_RETRY(close(fd)) < 0) {
+//			ERR("close");
+//		}
+//		while(fgets(buf, BUFF_SIZE, file[0]) != NULL) {
+//			sep_index = (int) (strchr(buf, COORDS_SEPARATOR) - buf);
+//			printf("Found %c on %d\n", COORDS_SEPARATOR, sep_index);
+//		}
+		for(i = 0; i < n*k; i++) {
+			locs[i].x = rand() % 10;
+			locs[i].y = rand() % 10;
+
+			printf("(%d, %d)\n", locs[i].x, locs[i].y);
+		}
+	}
+
+	if(rank == 0) {
+		printf("before scatter\n");
+	}
+	MPI_Scatter(locs, k, locationtype, rec, k, locationtype, 0, MPI_COMM_WORLD);
+	if(rank == 0) {
+		printf("after scatter\n");
+	}
+
+	for(i = 0; i < k; i++) {
+		printf("Poszukiwacz#%d: sprawdzam (%d, %d)...\n", rank, rec[i].x, rec[i].y);
+		sleep(rand() % (MAX_SLEEP - MIN_SLEEP) + MIN_SLEEP);
+		if( rand() % 5 != 0) {
+			printf("Poszukiwacz#%d: brak wraku w lokacji (%d, %d)\n", rank, rec[i].x, rec[i].y);
+			rec[i].x = 0;
+			rec[i].y = 0;
+		} else {
+			printf("Poszukiwacz#%d, znaleziono wrak w lokacji: (%d, %d)\n", rank, rec[i].x, rec[i].y);
+			++found_wrecks;
+		}
+		
+	}
+	
+	MPI_Reduce(&found_wrecks, &total_wrecks, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	
+	MPI_Gather(rec, k, locationtype, locs, k, locationtype, 0, MPI_COMM_WORLD);
+
+	/* Following barrier for the sake of clarity - it can be commented out */
+	MPI_Barrier(MPI_COMM_WORLD);
+	if( rank == 0) {
+		printf("Znaleziono wraków: %d\n", total_wrecks);
+		for(i = 0; i < n*k; i++) {
+			if(!(locs[i].x == 0 && locs[i].y == 0)) {
+				printf("Wrak %d: (%d, %d)\n", ++num, locs[i].x, locs[i].y);
+			}
+		}
+	}
+
+	MPI_Type_free(&locationtype);	
+	free(rec);
+	free(locs);
+	MPI_Finalize();
+	return 0;
+}
